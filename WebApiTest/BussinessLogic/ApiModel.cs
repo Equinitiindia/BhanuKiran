50608f3d512600937eb3bdf2d0b0470e51deadd4
BhanuKiran <techbhanukiran.p@gmail.com>
2018-10-27 13:17:54 +0530

    Code Commit webApi

---
+++ WebApiTest/BussinessLogic/ApiModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic
+{
+    public class ApiModel
+    {
+        public string Id { get; set; }
+        public int ApplicationId { get; set; }
+        public string Type { get; set; }
+        public string Summary { get; set; }
+        public double Amount { get; set; }
+        public string PostingDate { get; set; }
+        public bool IsCleared { get; set; }
+        public string ClearedDate { get; set; }
+
+
+
+        
+
+    }
+}
---
+++ WebApiTest/WebApiTest/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using BussinessLogic;
+using Services;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApiTest.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IHttpActionResult Get()
+        {
+            List<ApiModel> getApiModel = new List<ApiModel>();
+            WebApiServices webApiServices = new WebApiServices();
+            getApiModel = webApiServices.GetApiData().ToList();
+            if (getApiModel.Count > 0)
+            {
+                return Ok(getApiModel);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // GET api/values/5
+        public IHttpActionResult Get(int id)
+        {
+            List<ApiModel> getApiModelById = new List<ApiModel>();
+            WebApiServices webApiServices = new WebApiServices();
+            getApiModelById = webApiServices.GetApiDataById(id).ToList();
+            if (getApiModelById != null)
+            {
+                return Ok(getApiModelById);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        //// POST api/values
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
